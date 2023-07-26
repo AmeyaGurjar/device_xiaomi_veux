f5cb802e273c507e5c9b3b5ee8afc8877470e9e4
BlackCat <amey992004@gmail.com>
2023-07-26 22:55:19 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm -rf hardware/xiaomi
+rm -rf hardware/qcom-caf/sm8350/display
 git clone https://github.com/AmeyaGurjar/device_xiaomi_veux-kernel  device/xiaomi/veux-kernel
 git clone https://github.com/AmeyaGurjar/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 git clone https://gitlab.com/AmeyaGurjar/vendor_xiaomi_veux vendor/xiaomi/veux
