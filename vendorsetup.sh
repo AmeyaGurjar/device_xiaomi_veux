e934afb73fb5c5682e4c03b37c3767b5f4d30fe0
AmeyaGurjar <amey992004@gmail.com>
2023-07-27 14:58:25 +0530

    hardware/xiaomi fixed

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,5 @@ git clone https://github.com/AmeyaGurjar/device_xiaomi_veux-kernel  device/xiaom
 git clone https://github.com/AmeyaGurjar/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 git clone https://gitlab.com/AmeyaGurjar/vendor_xiaomi_veux vendor/xiaomi/veux
 git clone https://gitlab.com/AmeyaGurjar/vendor_xiaomi_veux-firmware vendor/xiaomi/veux-firmware
-git clone https://github.com/AmeyaGurjar/hardware_xiaomi hardware/xiaomi
+git clone https://github.com/PotatoDevices/hardware_xiaomi -b gnocchi-release hardware/xiaomi
 git clone https://github.com/AmeyaGurjar/hardware_qcom-caf_sm8350_display hardware/qcom-caf/sm8350/display
